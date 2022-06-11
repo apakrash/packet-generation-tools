975a20ae9882c80d49be14fd9a851eb3ea437eb0
abhishek pakrashi <ab.posts@gmail.com>
2022-06-11 06:52:13 +0530

    Create scapy-1-2-sr1.py

---
+++ code/scapy-1-2-sr1.py
@@ -0,0 +1,16 @@
+#phase: Ubuntu tested
+
+
+from scapy.all import *
+
+############ Section 2 sr1() ############
+
+#The sr1() function for getting 1 response packet back'''
+
+response_sr1 = sr1(IP(dst="1.1.1.1")/ICMP())
+print(response_sr1.summary())
+wrpcap("response_sr1.pcap", response_sr1)
+
+
+# Notice that the response can be saved in response_sr1.pcap. There is a sample response_sr1-sample.pcap present to check
+# To check the file, please use tcdump -r response_sr1.pcap or tcpdump -r response_sr1-sample.pcap  ## use sudo if normal tcpdump is not working
