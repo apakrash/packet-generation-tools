4882fdce07dd578a135f03af097469b54d4197cd
abhishek pakrashi <ab.posts@gmail.com>
2022-06-11 06:53:21 +0530

    Create scapy-1-1-sr.py

---
+++ code/scapy-1-1-sr.py
@@ -0,0 +1,13 @@
+#phase: Ubuntu tested
+
+
+from scapy.all import *
+
+
+############ Section 1 sr() ############
+
+'''The sr() function is used to send a packet or group of packets when you expect a response back.'''
+
+response_sr = sr(IP(dst="1.1.1.1")/ICMP())
+print(response_sr)
+#wrpcap("response_sr.pcap", response_sr) # this will throw an error
