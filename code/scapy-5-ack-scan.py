d0a3a1ab3f56a373f9c1d5ce09f93108a716308d
abhishek pakrashi <ab.posts@gmail.com>
2022-06-11 06:56:19 +0530

    Create scapy-5-ack-scan.py

---
+++ code/scapy-5-ack-scan.py
@@ -0,0 +1,18 @@
+from urllib import response
+from scapy.all import scapy
+from scapy.all import IP
+from scapy.all import ICMP
+from scapy.all import *
+
+#notice the change in flag
+
+#response= (sr1(IP(dst="10.197.225.120")/TCP(dport=443,flags="A")))
+
+#A noticed anomaly is that sr1 waits for the packet
+
+response= (sr(IP(dst="1.1.1.1")/TCP(dport=443,flags="A")))
+
+
+#print(dir(response))
+#print(response.packetfields())
+print(response)
