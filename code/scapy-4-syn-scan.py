4251ab8223c4963bfa6a5cb0b39f3956f49b672f
abhishek pakrashi <ab.posts@gmail.com>
2022-06-12 16:22:11 -0700

    Update scapy-4-syn-scan.py

--- code/scapy-4-syn-scan.py
+++ code/scapy-4-syn-scan.py
@@ -7,14 +7,16 @@ import time
 ############ SYN scan on 1 ip ############
 
 ip_address = ipaddress.IPv4Address('1.1.1.1')
-print(ip_address)
-print(type(str(ip_address)))
-print(int(ip_address))
-for i in range(5):
-    #time.wait(2)
-    print('TCP SYN scap for '+ str(ip_address))
+
+print('Starting SYN scan for ip address 1.1.1.1 to 1.1.1.3 for port 443 \n\n')
+
+for i in range(3):
+    time.sleep(2)
+    print('TCP SYN scap for ip = '+ str(ip_address))
     ip = str(ip_address)
     response= sr1(IP(dst=ip)/TCP(dport=443,flags="S")) # dont use str(ip_address) in this line, it breaks
-    print(response.show())
+    response.show()
     ip_address += 1
-    
+    print('***********************************************')
+    time.sleep(2)
+print('\n\n')
