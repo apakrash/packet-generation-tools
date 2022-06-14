985fbe98b3c86466c1631dbffbae1073e987d797
abhishek pakrashi <ab.posts@gmail.com>
2022-06-15 02:14:25 +0530

    Update scapy-6-ARP-ping.py

--- code/scapy-6-ARP-ping.py
+++ code/scapy-6-ARP-ping.py
@@ -5,9 +5,3 @@ from scapy.all import *
 ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst="1.1.1.1/32"), timeout=2)
 print(ans.summary(lambda s,r: r.sprintf("%Ether.src% %ARP.psrc%")))
 
-
-'''
-6c:41:6a:c7:b8:ff 1.1.1.1
-
-guess whose mac address address is this?
-'''
